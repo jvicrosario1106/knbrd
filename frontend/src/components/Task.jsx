74e169be60817f7e410877a049bc4038376188cc
JV Rosario <jvrosario@JVs-MacBook-Pro.local>
2022-04-20 13:04:32 +0800

    updated

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -4,7 +4,7 @@ import { FiTrash2 } from "react-icons/fi";
 
 const Task = ({ task, columnId }) => {
   return (
-    <div style={{width:"100%"}}>
+    <div style={{ width: "100%" }}>
       <Box sx={{ float: "right" }}>
         <IconButton
           size="small"
@@ -15,22 +15,27 @@ const Task = ({ task, columnId }) => {
       </Box>
       <Box>
         <Typography variant="body2" fontWeight={"bold"}>
-          {task.name.toString().substring(0,16) + "..."}
+          {task.name.toString().substring(0, 16) + "..."}
         </Typography>
-        <Typography variant="body2" sx={{mb:1, mt:2}}>
-          {task.description.toString().substring(0,60) + "..."}
+        <Typography variant="body2" sx={{ mb: 1, mt: 2 }}>
+          {task.description.toString().substring(0, 60) + "..."}
         </Typography>
       </Box>
 
       <Grid container spacing={1}>
-              {task.label.length > 0 && task.label.map(l=>(
-                <Grid item>
-                <Typography sx={{backgroundColor:l.color, color:"white", p:0.3}}  variant="body2" key={l._id}>
-                  {l.name}
-                </Typography>
-                </Grid>
-              ))}
-        </Grid>
+        {task.label.length > 0 &&
+          task.label.map((l) => (
+            <Grid item>
+              <Typography
+                sx={{ backgroundColor: l.color, color: "white", p: 0.3 }}
+                variant="body2"
+                key={l._id}
+              >
+                {l.name}
+              </Typography>
+            </Grid>
+          ))}
+      </Grid>
     </div>
   );
 };
